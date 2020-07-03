1f64230310bbcf693103aff82328b83137aaffa7
Jlin <zhangjialin@wisdomep.com>
2020-07-03 17:43:36 +0800

    zhangjialin

--- src/main/java/zhiyiting2/test/AuditTest.java
+++ src/main/java/zhiyiting2/test/AuditTest.java
@@ -1,7 +1,11 @@
 package zhiyiting2.test;
 
+import org.springframework.test.context.TestPropertySource;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import zhiyiting2.model.SqlModel;
+
+import java.util.List;
 
 
 /**
@@ -37,30 +41,29 @@ public class AuditTest extends ZTest{
 //@Parameters("auditType")
     public void normal_in_outTest(){
 
-    System.out.println("正常模式，入库   出库  开始=============================");
 
     AuditType.logger.info("正常模式  入库  出库 ");
     long serialId1 = System.currentTimeMillis() / 1000;
-    auditType.in(serialId1,plateNo1,"IN");
+    auditType.in(serialId1,plateNo,"IN");
     auditType.out(serialId1,"OUT");
-    System.out.println("正常模式--入库 出库  结束==============================");
+
 
 }
 @Test(testName = "正常模式，入库-入库")
 //    @Parameters("auditType")
     public void normal_in_inTest(){
-    System.out.println("正常模式，入库   入库  开始=============================");
+
     AuditType.logger.info("正常模式  入库  入库");
     long serialId1 = System.currentTimeMillis() / 1000;
     auditType.in(serialId1,plateNo,"IN");
     long serialId2 = System.currentTimeMillis() / 1000;
     auditType.in(serialId2,plateNo1,"IN");
-    System.out.println("正常模式--入库 入库  结束===============================");
+
 }
 @Test(testName = "正常模式，入库-出库-入库")
 
     public void normal_in_out_inTest(){
-    System.out.println("正常模式，入库-出库-入库  开始======================================");
+
     AuditType.logger.info("正常模式，入库-出库-入库");
     long serialId1 = System.currentTimeMillis() /1000;
 
@@ -68,7 +71,6 @@ public class AuditTest extends ZTest{
     auditType.out(serialId1,"OUT");
     long serialId2 = System.currentTimeMillis() / 1000;
     auditType.in(serialId2,plateNo1,"IN");
-    System.out.println("正常模式，入库-出库-入库  结束======================================");
 }
 
 //    @Test(testName = "正常模式，入库-出库-出库")
@@ -86,7 +88,7 @@ public class AuditTest extends ZTest{
     @Test(testName = "正常模式，入库-入库-出库")
 
     public void normal_in_in_outTest(){
-        System.out.println("正常模式，入库-入库-出库 开始======================================");
+
 
         AuditType.logger.info("正常模式，入库-入库-出库");
 
@@ -96,13 +98,12 @@ public class AuditTest extends ZTest{
         long serialId2 = System.currentTimeMillis() / 1000;
         auditType.in(serialId2,plateNo1,"IN");
         auditType.out(serialId2, "OUT");
-        System.out.println("正常模式，入库-出库-入库  结束======================================");
+
     }
 
 
     @Test(testName = "正常模式，入库-不出库-入库")
     public void normal_in_noout_inTest(){
-        System.out.println("正常模式，入库-不出库-入库  开始========================");
 
         AuditType.logger.info("正常模式，入库-不出库-入库");
         long serialId1 = System.currentTimeMillis() / 1000;
@@ -111,13 +112,12 @@ public class AuditTest extends ZTest{
         long serialId2 = System.currentTimeMillis() / 1000;
         auditType.in(serialId2,plateNo1,"IN");
 
-        System.out.println("正常模式，入库-不出库-入库  结束========================");
 
     }
 
     @Test(testName = "正常模式，入库-不出库-出库")
     public void normal_in_noout_outTest(){
-        System.out.println("正常模式，入库-不出库-出库  开始========================");
+
 
         AuditType.logger.info("正常模式，入库-不出库-出库");
         long serialId1 = System.currentTimeMillis() / 1000;
@@ -126,62 +126,219 @@ public class AuditTest extends ZTest{
         long serialId2 = System.currentTimeMillis() / 1000;
         auditType.out(serialId2,"OUT");
 
-        System.out.println("正常模式，入库-不出库-出库  结束========================");
 
     }
 
     @Test(testName = "定时模式, 入库-出库")
     public void period_in_outTest(){
-        System.out.println("定时模式，入库-出库  开始========================");
 
         AuditType.logger.info("定时模式，入库-出库");
         long serialId1 = System.currentTimeMillis() / 1000;
-        auditType.period_in(serialId1, plateNo, "PERIOD_IN");
-        auditType.period_out(serialId1,"PERIOD_OUT");
+        auditType.period(serialId1, plateNo, "PERIOD_IN");
 
-        System.out.println("定时模式，入库-出库  结束=====================================");
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId2,null,"PERIOD_OUT");
+
+
 
     }
 
     @Test(testName = "定时模式，入库-保持不变-出库")
     public void period_in_keep_in(){
-        System.out.println("定时模式，入库-保持不变-出库  开始==================================");
         AuditType.logger.info("定时模式，入库-保持不变-出库");
         long serialId1 = System.currentTimeMillis() / 1000;
-        auditType.period_in(serialId1,plateNo,"PERIOD_IN");
-        auditType.period_out(serialId1,"PERIOD_KEEP");
-        auditType.period_out(serialId1,"PERIOD_OUT");
-        System.out.println("定时模式，入库-保持不变-出库  结束=================================");
+        auditType.period(serialId1,plateNo,"PERIOD_IN");
+
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId2,null,"PERIOD_KEEP");
+
+        long serialId3 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId3,null,"PERIOD_OUT");
 
     }
+
+    @Test(testName = "定时模式-入库-保持不变，正常模式-出库")
+    public void period_in_keep_normalinTest(){
+
+        AuditType.logger.info("定时模式-入库-保持不变，正常模式-出库");
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId1,plateNo,"PERIOD_IN");
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId1,null,"PERIOD_KEEP");
+        long serialId3 = System.currentTimeMillis() / 1000;
+        auditType.out(serialId3,"OUT");
+
+
+    }
+
 
     @Test(testName = "定时模式-入库-保持不变，正常模式-入库")
-    public void period_in_keep_normalinTest(){
-        System.out.println("定时模式-入库-保持不变，正常模式-入库  开始==============================");
+    public void period_in_keep_normalin(){
+    AuditType.logger.info("定时模式-入库-保持不变，正常模式-入库");
+    long serialId1 = System.currentTimeMillis() / 1000;
+    auditType.period(serialId1,plateNo,"PERIOD_IN");
 
-        AuditType.logger.info("定时模式-入库-保持不变，正常模式-入库");
+    long serialId2 = System.currentTimeMillis() / 1000;
+    auditType.period(serialId2,null,"PERIOD_KEEP");
+
+    long serialId3 = System.currentTimeMillis() / 1000;
+    auditType.in(serialId3,plateNo1,"IN");
+    }
+
+
+    @Test(testName = "正常模式-入库-合并")
+    public void normal_in_inMerge(){
+        AuditType.logger.info("正常模式-入库-合并");
         long serialId1 = System.currentTimeMillis() / 1000;
-        auditType.period_in(serialId1,plateNo,"PERIOD_IN");
-        auditType.period_out(serialId1,"PERIOD_KEEP");
+        auditType.in(serialId1,plateNo,"IN");
         long serialId2 = System.currentTimeMillis() / 1000;
-        auditType.out(serialId2,"OUT");
-        System.out.println("定时模式-入库-保持不变，正常模式-入库  结束==============================");
+        auditType.in(serialId2,null,"IN_MERGE");
 
     }
 
-    @Test
-    public void period_in_keep_out(){
-        System.out.println();
-        AuditType.logger.info("定时模式-入库-保持不变-出库");
-        long serialId = System.currentTimeMillis() / 1000;
+	@Test(testName="正常模式-入库-确认不出库，定时模式-保持不变，正常模式-确认不出库-合并")
+    public void normal_in_noout_PeriodKeep_normal_noout_inmerge(){
+        AuditType.logger.info("正常模式-入库-确认不出库，定时模式-保持不变，正常模式-确认不出库-合并");
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId1,plateNo,"IN");
+        auditType.out(serialId1,"NO_OUT");
 
-        auditType.period_in(serialId,plateNo,"PERIOD_IN");
-        auditType.period_out(serialId,"PERIOD_KEEP");
-        auditType.period_out(serialId,"PERIOD_OUT");
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId2,null,"PERIOD_KEEP");
+
+        long serialId3 = System.currentTimeMillis() / 1000;
+        auditType.out(serialId3,"NO_OUT");
+
+        long serialId4 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId4,null,"IN_MERGE");
     }
 
-	
+    @Test(testName = "定时模式-入库，正常模式-合并")
+    public void period_in_normalKeep(){
+        AuditType.logger.info("定时模式-入库，正常模式-合并");
 
-	
-	
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId1,plateNo,"PERIOD_IN");
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId2,null,"IN_MERGE");
+    }
+
+    @Test(testName = "定时模式-入库，正常模式-确认不出库-合并")
+    public void period_in_normal_noout_inmerge(){
+        AuditType.logger.info("定时模式-入库，正常模式-确认不出库-合并");
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId1,plateNo,"PERIOD_IN");
+        auditType.out(serialId1,"NO_OUT");
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId2,null,"IN_MERGE");
+    }
+
+    @Test(testName = "定时模式-入库-保持不变，正常模式-合并")
+    public void period_in_keep_normal_inMerge() {
+        AuditType.logger.info("定时模式-入库-保持不变，正常模式-合并");
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId1, plateNo, "PERIOD_IN");
+        auditType.period(serialId1, null, "PERIOD_KEEP");
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId2,null,"IN_MERGE");
+
+
+    }
+
+
+    @Test(testName = "定时模式-入库-保持不变，正常模式-确认不出库，定时模式-保持不变")
+    public void period_in_keep_normal_noout_period_keep(){
+        AuditType.logger.info("定时模式-入库-保持不变，正常模式-确认不出库，定时模式-保持不变");
+
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId1,plateNo,"PERIOD_IN");
+
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId2,null,"PERIOD_KEEP");
+
+        long serialId3 = System.currentTimeMillis() / 1000;
+        auditType.out(serialId3,"NO_OUT");
+
+        long serialId4 = System.currentTimeMillis() / 1000;
+        auditType.period(serialId4,null,"PERIOD_KEEP");
+    }
+
+    @Test(testName = "正常模式-入库-确认不出库-合并")
+    public void normal_in_noout_inMerge(){
+        AuditType.logger.info("正常模式-入库-确认不出库-合并");
+
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId1,plateNo,"IN");
+        auditType.out(serialId1,"NO_OUT");
+
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId2,null,"IN_MERGE");
+    }
+
+
+    @Test(testName = "自动入库-自动确认不出库-自动合并-自动出库")
+    public void auto_in_noout_inMerge(){
+
+        AuditType.logger.info("自动入库-自动确认不出库-自动合并-自动出库（识别不到车牌)");
+//
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.auto_in(serialId1);
+        auditType.auto_noout(serialId1);
+        long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.auto_in(serialId2);
+        auditType.auto_out(serialId2);
+    }
+
+
+    @Test(testName = "自动入库，手动出库")
+    public void auto_in_manual_out(){
+
+        //手动出库---(出库识别的车牌与最近一条入库识别的车牌不一致,无法自动出库)
+        AuditType.logger.info("自动入库，手动出库");
+        Long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.auto_in(serialId1);
+
+        //自动入库的时候，只有自动出库失败，也就是识别到的图片无法自动出库，也无法自动不出库，才能手动处理
+        //失败情况模拟：自动入库车牌号为A，出库图片识别到的车牌号为B
+        auditType.autoOutHandleFail_mannulHandle(serialId1,"OUT");
+
+
+    }
+
+    @Test(testName = "自动入库，手动不出库，手动合并，自动出库")
+    public void auto_in_mannual_noout_mannul_inMerge_auto_out() {
+
+        AuditType.logger.info("自动入库，手动不出库，手动合并，自动出库");
+
+        long serialId1 = System.currentTimeMillis() / 1000;
+        auditType.auto_in(serialId1);
+        auditType.autoOutHandleFail_mannulHandle(serialId1, "NO_OUT");
+        Long serialId2 = System.currentTimeMillis() / 1000;
+        auditType.in(serialId2, null, "IN_MERGE");
+
+        auditType.auto_out(serialId2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/zhiyiting2/test/AuditType.java
+++ src/main/java/zhiyiting2/test/AuditType.java
@@ -35,8 +35,6 @@ public class AuditType extends ZTest {
                         null);
 
             }
-            System.out.println("========================入库审核结束==========================");
-
         } catch (Exception e) {
             e.printStackTrace();
             logger.error(e);
@@ -50,6 +48,11 @@ public class AuditType extends ZTest {
             logger.info("上报出库");
             deviceService.reportOut(deviceNo, "OUT", 63, 21,
                     false, serialId, System.currentTimeMillis() / 1000);
+
+            logger.info("上报出库图片");
+            deviceService.uploadDeviceOutFile(35, 20, 62,
+                    8, deviceNo, serialId, System.currentTimeMillis() / 1000, "0",
+                    "C:\\Users\\DELL\\Desktop\\停车自造图片\\出库\\p0.png");
             for (int i = 0; i < 4; i++) {
                 //出库过程图片
                 logger.info("上报出库过程图片");
@@ -58,10 +61,7 @@ public class AuditType extends ZTest {
                         "C:\\Users\\DELL\\Desktop\\停车自造图片\\出库过程\\p" + i + ".png");
                 Thread.sleep(1000);
             }
-            logger.info("上报出库图片");
-            deviceService.uploadDeviceOutFile(35, 20, 62,
-                    8, deviceNo, serialId, System.currentTimeMillis() / 1000, "0",
-                    "C:\\Users\\DELL\\Desktop\\停车自造图片\\出库\\p0.png");
+
             logger.info("确认出库");
             List<Object> objOut = jdbconn
                     .query("select a.id from audit a where a.audit_type='OUT' and a.serial_id='"
@@ -78,10 +78,9 @@ public class AuditType extends ZTest {
         }
     }
 
-    public void period_in(Long serialId, String plateNo, String priodInHandleType) {
+    public void period(Long serialId, String plateNo, String priodAuditHandleType) {
         try {
-            logger.info("定时模式--入库");
-
+            logger.info("定时模式");
             logger.info("定时模式上报");
             deviceService.uploadDevicePeriodModeFile(deviceNo, sdf.format(new Date()),
                     serialId, System.currentTimeMillis() / 1000, "C:\\Users\\DELL\\Desktop\\出入库测试图片\\p5.jpg");
@@ -89,12 +88,11 @@ public class AuditType extends ZTest {
                     + serialId.intValue() + "' order by id desc", SqlModel.class);
             if (obj != null && obj.size() > 0) {
                 Integer auditId_in = SqlModel.class.cast(obj.get(0)).getId();
-                logger.info("定时入库审核--auditId_in:" + auditId_in);
-                parkingService.manualHandleAudit(plateNo, false, false, "SMALL", auditId_in, priodInHandleType,
+                logger.info("定时审核--auditId_in:" + auditId_in);
+                parkingService.manualHandleAudit(plateNo, false, false, "SMALL", auditId_in, priodAuditHandleType,
                         null);
             }
-
-            System.out.println("================================定时模式入库审核结束=============================");
+            logger.info("定时模式审核结束");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -103,18 +101,23 @@ public class AuditType extends ZTest {
 
     }
 
-    public void period_out(Long serialId,String periodOutHandleType){
+    public void auto_in(Long serialId){   //,String autoAuditHandleType
         try {
-            logger.info("定时模式上报");
-            deviceService.uploadDevicePeriodModeFile(deviceNo, sdf.format(new Date()),
-                    serialId, System.currentTimeMillis() / 1000, "C:\\Users\\DELL\\Desktop\\出入库测试图片\\p7.jpg");
-            List<Object> obj1 = jdbconn.query("select a.id from audit a where a.audit_type='PERIOD' and a.serial_id='"
-                    + serialId.intValue() + "' order by id desc", SqlModel.class);
-            if(obj1 != null && obj1.size() > 0) {
-                Integer auditId_out = SqlModel.class.cast(obj1.get(0)).getId();
-                logger.info("定时审核--auditId_out:"+auditId_out);
-                parkingService.manualHandleAudit(null, null, null, null, auditId_out, periodOutHandleType, null);
+
+            logger.info("上报入库");
+            deviceService.reportIn(deviceNo, "PREPARE", 12, 25, serialId,
+                    System.currentTimeMillis() / 1000);
+            // 设备上传8张图片
+            for (int i = 1; i < 9; i++) {
+                logger.info("上报入库图片");
+                deviceService.uploadDeviceFile(35, 20, 61, 1, 8, deviceNo,
+                        serialId, System.currentTimeMillis() / 1000, "C:\\Users\\DELL\\Desktop\\自动出入库图片\\入库\\p" + i + ".png");
+                //C:\Users\DELL\Desktop
+                Thread.sleep(1000);
             }
+
+            logger.info("自动入库结束");
+
         } catch (Exception e) {
             e.printStackTrace();
             logger.error(e);
@@ -123,10 +126,98 @@ public class AuditType extends ZTest {
     }
 
 
+    public void auto_noout(Long serialId) {
+        try {
+            //入库审核后 传取证图 出库过程图片 出库  出库图片 1为出库过程  0为出库图片
+            //报出库
+            logger.info("上报出库");
+            deviceService.reportOut(deviceNo, "OUT", 63, 21,
+                    false, serialId, System.currentTimeMillis() / 1000);
+
+            logger.info("上报出库图片");
+            deviceService.uploadDeviceOutFile(35, 20, 62,
+                    8, deviceNo, serialId, System.currentTimeMillis() / 1000, "0",
+                    "C:\\Users\\DELL\\Desktop\\自动出入库图片\\出库\\p1.png");
+
+            for (int i = 0; i < 4; i++) {
+                //出库过程图片
+                logger.info("上报出库过程图片");
+                deviceService.uploadDeviceOutFile(35, 20, 62, 8, deviceNo, serialId,
+                        System.currentTimeMillis() / 1000, "1", "C:\\Users\\DELL\\Desktop\\自动出入库图片\\出库过程\\p" + i + ".png");
+                Thread.sleep(1000);
+            }
+
+            logger.info("确认不出库");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e);
+        }
+    }
+
+    public void auto_out(Long serialId) {
+        try {
+            //入库审核后 传取证图 出库过程图片 出库  出库图片 1为出库过程  0为出库图片
+            //报出库
+            logger.info("上报出库");
+            deviceService.reportOut(deviceNo, "OUT", 63, 21,
+                    false, serialId, System.currentTimeMillis() / 1000);
+            logger.info("上报出库图片");
+            deviceService.uploadDeviceOutFile(35, 20, 62,
+                    8, deviceNo, serialId, System.currentTimeMillis() / 1000, "0",
+                    "C:\\Users\\DELL\\Desktop\\自动出入库图片\\出库\\p2.png");
+            for (int i = 0; i < 4; i++) {
+                //出库过程图片
+                logger.info("上报出库过程图片");
+                deviceService.uploadDeviceOutFile(35, 20, 62, 8, deviceNo, serialId,
+                        System.currentTimeMillis() / 1000, "1", "C:\\Users\\DELL\\Desktop\\自动出入库图片\\出库过程\\p" + i + ".png");
+                Thread.sleep(1000);
+            }
+
+
+            logger.info("确认出库");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e);
+        }
+    }
 
 
 
 
+    public void autoOutHandleFail_mannulHandle(Long serialId,String outHandleType) {
+        try {
+            logger.info("上报出库");
+            deviceService.reportOut(deviceNo, "OUT", 63, 21,
+                    false, serialId, System.currentTimeMillis() / 1000);
+            for (int i = 0; i < 4; i++) {
+                //出库过程图片
+                logger.info("上报出库过程图片");
+                deviceService.uploadDeviceOutFile(35, 20, 62, 8, deviceNo, serialId,
+                        System.currentTimeMillis() / 1000, "1", "C:\\Users\\DELL\\Desktop\\自动出入库图片\\出库过程\\p" + i + ".png");
+                Thread.sleep(1000);
+            }
+            AuditType.logger.info("上报出库图片");
+            deviceService.uploadDeviceOutFile(35, 20, 62,
+                    8, deviceNo, serialId, System.currentTimeMillis() / 1000, "0",
+                    "C:\\Users\\DELL\\Desktop\\自动出入库图片\\出库\\p9.png");  //p9.png为另一张车牌的图片
+            AuditType.logger.info("确认出库");
+            List<Object> objOut = jdbconn
+                    .query("select a.id from audit a where a.audit_type='OUT' and a.serial_id='"
+                            + serialId.intValue() + "' order by id desc", SqlModel.class);
+            if (objOut != null && objOut.size() > 0) {
+                Integer auditIdOut = SqlModel.class.cast(objOut.get(0)).getId();
+                parkingService.manualHandleAudit(null, null, null, null, auditIdOut, outHandleType, null);
+            }
+
+            logger.info("确认出库");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
